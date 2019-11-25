3f621905b8da8581d87dfd202f0248e8f8183a1e
ChefIronBelly <werthmation@gmail.com>
2019-11-25 07:16:45 -0500

    adds and tweaks

--- chef/.config/wal/templates/colors-wal-dwm.h
+++ chef/.config/wal/templates/colors-wal-dwm.h
@@ -1,5 +1,5 @@
 static const char norm_fg[] = "{color15}";
-static const char norm_bg[] = "{color0}";
+static const char norm_bg[] = "{color1}";
 static const char norm_border[] = "{color8}";
 
 static const char sel_fg[] = "{color15}";
