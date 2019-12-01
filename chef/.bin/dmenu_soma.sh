f7473af83bcb2041dc3070fc817eaf682f0c3ce9
ChefIronBelly <werthmation@gmail.com>
2019-12-01 11:27:47 -0500

    adds and tweaks

--- chef/.bin/dmenu_soma.sh
+++ chef/.bin/dmenu_soma.sh
@@ -7,19 +7,19 @@ selection="";
 
 if [ "$(pgrep mpv)" ] ; then
         #pop.sh "All your sound belong to us ..." # re:lemonbar, wmcore.
-        echo "All your sound belong to us ..." | dmenu -nb "$color0" -nf "$color15" -sb "$color7" -sf "$color15" 
+        echo "All your sound belong to us ..." | dmenu -nb "$color0" -nf "$color15" -sb "$color4" -sf "$color15" 
         pkill mpv
         exit 0
 fi
 
 stations="$(cat /home/chef/.bin/stations)"
-selection="$(echo "$stations" | dmenu "$@" -nb "$color0" -nf "$color15" -sb "$color7" -sf "$color15")"
+selection="$(echo "$stations" | dmenu "$@" -nb "$color0" -nf "$color15" -sb "$color4" -sf "$color15")"
 #player() { mpg123 -C -@ "$@" 2>/dev/null & }
 player () { mpv "$@" 2>/dev/null &} 
 
 if [ "$selection" ]; then
 		#pop.sh "Playing SomaFM channel $selection ..."
-		echo "Playing SomaFM channel $selection ..." | dmenu -nb "$color0" -nf "$color15" -sb "$color7" -sf "$color15" 
+		echo "Playing SomaFM channel $selection ..." | dmenu -nb "$color0" -nf "$color15" -sb "$color4" -sf "$color15" 
 		player http://somafm.com/$selection.pls
 else exit;
 fi
--- chef/.config/wal/templates/colors-wal-dwm.h
+++ chef/.config/wal/templates/colors-wal-dwm.h
@@ -1,10 +1,10 @@
-static const char norm_fg[] = "{color7}";
+static const char norm_fg[] = "{color4}";
 static const char norm_bg[] = "{color0}";
 static const char norm_border[] = "{color0}";
 
 static const char sel_fg[] = "{color15}";
-static const char sel_bg[] = "{color7}";
-static const char sel_border[] = "{color7}";
+static const char sel_bg[] = "{color4}";
+static const char sel_border[] = "{color4}";
 
 static const char *colors[][3]      = {{
     /*               fg           bg         border                         */
